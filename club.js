62d850df6dafeb223e70342ee178641fcdf981cb
harshxll <131367881+harshxll@users.noreply.github.com>
2023-09-10 03:02:33 +0530

    Update club.js

--- club.js
+++ club.js
@@ -1,6 +1,28 @@
-import LocomotiveScroll from 'locomotive-scroll';
 
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
-});
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true
+    });
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        grabCursor:true,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+const cursor=document.querySelector('.cursor');
+const main=document.querySelector('.main');
+document.addEventListener("mousemove",function(prop){
+   cursor.style.left=(prop.x-100)+"px";
+   cursor.style.top=(prop.y-100)+"px";
+})
